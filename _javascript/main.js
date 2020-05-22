c7ed9927a5588715f868fb8d4ca9db6932c2b6d0
Sebastian <37258153+PenguinDesign@users.noreply.github.com>
2020-05-22 18:33:13 -0500

    City compelte

---
+++ _javascript/main.js
@@ -0,0 +1,64 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach(el => {
+      el.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        //change nav color
+        if (isScrolled) {
+          navbar.style.backgroundColor = primary;
+        } else {
+          if (isClicked === false) {
+            navbar.style.backgroundColor = primary;
+            isClicked = true;
+          } else if (isClicked) {
+            navbar.style.backgroundColor = 'transparent';
+            isClicked = false;
+          }
+        }
+
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+});
+var isClicked = false;
+var isScrolled = false;
+
+var primary = '#9f00a7';
+
+var navbar = document.getElementById('nav');
+
+window.addEventListener("scroll", function (event) {
+  var deviceHeight = window.screen.height * 0.9;
+  var scroll = this.scrollY;
+
+  if (scroll > deviceHeight) {
+    isScrolled = true;
+    navbar.style.backgroundColor = primary;
+
+  } else if (scroll < deviceHeight) {
+    if (isClicked) {
+      navbar.style.backgroundColor = primary;
+    } else {
+      navbar.style.backgroundColor = 'transparent';
+      isScrolled = false;
+    }
+  }
+
+});
